1e7ff73091249c40d0c5e425e7376b59a210cfa3
xbayazid <bayazid9006@gmail.com>
2023-11-19 20:39:18 +0600

    resume link added

--- src/components/Buttons/CvButton.js
+++ src/components/Buttons/CvButton.js
@@ -4,7 +4,7 @@ import arrowImg from '../../assets/arrow.png'
 const CvButton = () => {
     return (
         <div>
-            <a target="_blank" href="https://drive.google.com/file/d/1GO0ded2PFHvlFkRPVop9MgQQAfzTgJUw/view?usp=share_link" class="relative inline-flex items-center justify-center  px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-emerald-300 rounded-full shadow-md group">
+            <a target="_blank" href="https://drive.google.com/file/d/11tEmBewxGi_74uQjk4UUDZQN4x5VH06b/view?usp=sharing" class="relative inline-flex items-center justify-center  px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-emerald-300 rounded-full shadow-md group">
                 <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-emerald-300 group-hover:translate-x-0 ease">
                     <img className='w-7' src={arrowImg} alt="" />
                 </span>
